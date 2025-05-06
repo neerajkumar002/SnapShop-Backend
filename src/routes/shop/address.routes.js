07491f273e12920c1bcc6f2758d3d5882fc16f52
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-05-06 13:09:55 +0530

    fix: update address routes for consistency and clarity

--- src/routes/shop/address.routes.js
+++ src/routes/shop/address.routes.js
@@ -1,14 +1,18 @@
 import express from "express";
 import {
   addAddress,
+  deleteAddress,
   fetchAllAddresses,
   getAddress,
+  updateAddress,
 } from "../../controllers/shop/address.controller.js";
 
 const route = express.Router();
 
 route.post("/add", addAddress);
-route.get("/:userId", fetchAllAddresses);
-route.get("/:addressId", getAddress);
+route.get("/list/:userId", fetchAllAddresses);
+route.get("/:id", getAddress);
+route.put("/update/:userId/:addressId", updateAddress);
+route.delete("/delete/:userId/:addressId", deleteAddress);
 
 export default route;
