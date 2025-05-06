da7bef96613e740d3eabeb9276bddc54e5b9929f
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-05-06 13:34:04 +0530

    fix: update cookie security setting in login response

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -74,7 +74,7 @@ const login = async (req, res) => {
       }
     );
 
-    return res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    return res.cookie("token", token, { httpOnly: true, secure: true }).json({
       success: true,
       message: "Logged in successfully!",
       user: {
